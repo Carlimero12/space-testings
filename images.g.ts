25f75bc74814d9f1a014e2e1b1b9fbe3140d73bf
Carlimero12 <carlimerogaming@gmail.com>
2024-12-20 13:06:43 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ images.g.ts
@@ -0,0 +1,26 @@
+// Automatisch generierter Code. Nicht bearbeiten.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Automatisch generierter Code. Nicht bearbeiten.
